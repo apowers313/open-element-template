03f6ffb7fb9a5d6ef9ce0ee6cd836c9bcbf7f6c4
Adam Powers <apowers@ato.ms>
2015-09-02 07:34:07 -0700

    Testing deploy pages

---
+++ deploy-docs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# https://github.com/settings/tokens
+GH_TOKEN=b29d7ea4479b4a803292b4ab4a1610ec578793b1
+
+if [ -z "$GH_TOKEN" ]; then
+	echo "GH_TOKEN not set, please visit https://github.com/settings/tokens to get a token and then set the environment variable"
+	exit 1
+fi
+
+if [ -z "$TRAVIS_REPO_SLUG" ]; then
+	TRAVIS_REPO_SLUG=`git config --get remote.origin.url | cut -d/ -f4-5 | cut -d. -f1`
+fi
+
+if [ -z "$TRAVIS_REPO_SLUG" ]; then
+	echo "TRAVIS_REPO_SLUG is not set. Set it in the form \"<username>/<repo>\""
+	exit 1
+fi
+
+GH_REF=github.com/$(TRAVIS_REPO_SLUG).git
+if [ -z "$GH_REF" ]; then
+	echo "GH_REF is not set. Set it in the form \"github.com/<username>/<repo>.git\""
+	exit 1
+fi
+
+echo "Building docs ..."
+npm run docs
+
+echo "Uploading docs to: " $(GH_REF) ...
+
+# go to the out directory and create a new git repo
+cd docs
+git init
+
+# inside this git repo we'll pretend to be a new user
+git config user.name "Adam Powers"
+git config user.email "apowers@ato.ms"
+git add .
+
+# The first and only commit to this new Git repo contains all the
+# files present with the commit message "Deploy docs to GitHub Pages".
+git commit -m "Deploy docs to GitHub Pages"
+
+# Force push from the current repo's master branch to the remote
+# repo's gh-pages branch. (All previous history on the gh-pages branch
+# will be lost, since we are overwriting it.) We redirect any output to
+# /dev/null to hide any sensitive credential data that might otherwise be exposed.
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+
+# http(s)://<username>.github.io/<projectname>
