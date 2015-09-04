5327e8606243161b33b1aac3f3cc62b663b474a6
Adam Powers <apowers@ato.ms>
2015-09-04 09:25:56 -0700

    Updating documentation

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 /**
-This is just a helper for initializing the testProperty. I was using it to debug Istanbul.
-
-@class getHelloWorldString
+ * This is just a helper for initializing the testProperty. 
+ * Since Istanbul currently only supports .js files (and not .html), only this code will show up in the code coverage report.
+ *
+ * @class getInitialValue
 */
 function getInitialValue() {
 	console.log ("Hello from test.js");
