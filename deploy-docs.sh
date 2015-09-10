c61bc23fa512e7c69ed36f331d67314039bbf1f4
Adam Powers <apowers@ato.ms>
2015-09-10 15:54:43 -0700

    Debugging deploy docs

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# https://github.com/settings/tokens
 
+echo "Deploy Docs: Checking GitHub token..."
 if [ -z "$GH_TOKEN" ]; then
 	echo "GH_TOKEN not set, please visit https://github.com/settings/tokens to get a token and then set the environment variable"
 	exit 1
 fi
 
+echo "Deploy Docs: Checking Travis repo..."
 if [ -z "$TRAVIS_REPO_SLUG" ]; then
 	TRAVIS_REPO_SLUG=`git config --get remote.origin.url | cut -d/ -f4-5 | cut -d. -f1`
 fi
@@ -14,6 +15,7 @@ if [ -z "$TRAVIS_REPO_SLUG" ]; then
 	TRAVIS_REPO_SLUG='{{github_slug}}'
 fi
 
+echo "Deploy Docs: Setting GitHub ref..."
 GH_REF=github.com/$TRAVIS_REPO_SLUG.git
 if [ -z "$GH_REF" ]; then
 	echo "GH_REF is not set. Set it in the form \"github.com/<username>/<repo>.git\""
@@ -22,37 +24,40 @@ fi
 
 # If this is the root repository, use the default docs
 if [ "$INIT_DEFAULTS" == "1" ]; then
-	echo "USING DEFAULT DOCUMENTS"
-	git checkout master
+	echo "Deploy Docs: USING DEFAULT DOCUMENTS..."
+	git checkout -f
 fi
 
-echo "Building docs ..."
+echo "Deploy Docs: Building docs ..."
 npm run docs
 
 if [ -d coverage ]; then
-	echo "Moving coverage ..."
+	echo "Deploy Docs: Moving coverage ..."
 	mv coverage docs
 fi
 
-echo "Uploading docs to: " $GH_REF ...
+echo "Deploy Docs: Uploading docs to: " $GH_REF ...
 
 # go to the out directory and create a new git repo
 cd docs
 git init
 
 # inside this git repo we'll pretend to be a new user
+echo "Deploy Docs: Doing git config..."
 git config user.name "{{git_username}}"
 git config user.email "{{git_email}}"
 git add .
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy docs to GitHub Pages".
+echo "Deploy Docs: Doing git commit..."
 git commit -m "Deploy docs to GitHub Pages"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+echo "Deploy Docs: pushing docs to GitHub"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 
-# http(s)://<username>.github.io/<projectname>
+echo "Deploy Docs: done."
