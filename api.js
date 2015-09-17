bc978a0a19d85254e531ad2062cf23242558b698
Adam Powers <apowers@ato.ms>
2015-09-17 11:27:33 -0700

    Deploy docs to GitHub Pages from build

---
+++ api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "getInitialValue",
+        "{{element_name}}"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
